4a01db4a867987a4185e23a2cc96d0e0bc1e7116
Vladimir Sima <sima@esten.sk>
2025-03-17 21:12:37 +0100

    initial release

---
+++ stirling_pdf/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv bashio
+
+enable_security=$(bashio::config 'enable_security')
+
+bashio::log.info "Enable Security: ${enable_security}"
+
+exec java -Dfile.encoding=UTF-8 -jar /app.jar
