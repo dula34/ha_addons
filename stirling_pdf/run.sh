52e155fb1d10cb2883f4abd5539dc9459821639a
Vladimir Sima <sima@esten.sk>
2025-03-17 22:08:58 +0100

    add tini

--- stirling_pdf/run.sh
+++ stirling_pdf/run.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 
-enable_security=$(bashio::config 'enable_security')
+##!/usr/bin/with-contenv bashio
 
-bashio::log.info "Enable Security: ${enable_security}"
+#enable_security=$(bashio::config 'enable_security')
+
+#bashio::log.info "Enable Security: ${enable_security}"
 
 exec java -Dfile.encoding=UTF-8 -jar /app.jar
